9eb89930b804a574e2a27f47b45e6ccc33987533
miloiu <miloius@yahoo.com>
2024-10-12 15:32:05 +0300

    Initial Commit

---
+++ Student Assistant/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Student Assistant
+//
+//  Created by Stefan Miloiu on 12.10.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Student Assistant/Student_AssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  Student_AssistantApp.swift
+//  Student Assistant
+//
+//  Created by Stefan Miloiu on 12.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct Student_AssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
